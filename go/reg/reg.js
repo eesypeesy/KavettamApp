8d6b341e0978fb1d3663de8810d04d86cc0e7247
eesypeesy <46086523+eesypeesy@users.noreply.github.com>
2020-05-19 16:42:41 +0530

    tuning for width > 500 💻

--- go/reg/reg.js
+++ go/reg/reg.js
@@ -1,37 +1,29 @@
 
 const bar = document.getElementById("menubar");
 const menuName = document.getElementById("menuname");
-
 const bottom = document.getElementById("bod");
-
 bar.onmouseover = () => {
+  if(window.innerWidth < 500){
   menuName.style.opacity = ".1";
-  bottom.style.marginTop = "20%";
+  bottom.style.marginTop = "20%";}
 }
 bar.onmouseout = () => {
-
+  if(window.innerWidth < 500){
   menuName.style.opacity = "1";
-  bottom.style.marginTop = "0%";
+  bottom.style.marginTop = "0%";}
 }
-
 function sendProblem() {
   window.open("https://api.whatsapp.com/send?phone=+918281058094&text=SFI%20Online%20Kalotsavam%20Registration%20problem%20:%20%22your%20problem%20here%22");
-}
-function Branch() {
+}function Branch() {
   document.getElementById("branchlist").style.display = "block";
-}	
-function checkBranch(arg) {
+}	function checkBranch(arg) {
   const a= String(arg);
   let b = document.getElementById(a).innerHTML;
   document.getElementById("selectBranch").value = b;
   document.getElementById("branchlist").style.display = "none";
-}	
-
-
-function Event() {
+}	function Event() {
   document.getElementById("eventlist").style.display = "inline";
-}
-function checkEvent(arg) {
+}function checkEvent(arg) {
   const a= String(arg);
   let b = document.getElementById(a).innerHTML;
   document.getElementById("selectEvent").value = b;
--- main.js
+++ main.js
@@ -3,20 +3,20 @@ const bar = document.getElementById("menuname");
 const header = document.getElementById("menuhead");
 const yorum = document.getElementById("yorum");
 const image = document.getElementById("image");
-
 bar.onmouseover = () => {
+  if(window.innerWidth < 500){
   image.style.opacity = ".5";
-
   //menuName.style.opacity = ".3";
   header.style.opacity = ".3";
   yorum.style.marginTop = "30%";
+  }
 }
 bar.onmouseout = () => {
+  if(window.innerWidth < 500){
   image.style.opacity = ".9";
-
   //menuName.style.opacity = "1";
   header.style.opacity = "1"; 
-   yorum.style.marginTop = "20%";
+   yorum.style.marginTop = "20%";}
 
 }
 
